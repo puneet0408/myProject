76d5dbd6fce7d98c489d37c27ea61d7c98b1e19e
puneet0408 <punitvirmani725@gmail.com>
2022-04-25 22:26:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// let menu = document.querySelector("#menu-bars");
+// let navbar = document.querySelector(".nav");
+
+// menu.onclick = () => {
+//   menu.classList.toggle("fa-times");
+//   navbar.classList.toggle("active");
+// };
+
+const nameInput = document.getElementById("name")
+const email = document.getElementById("email")
+const message = document.getElementById("message")
+const sucess = document.getElementById("sucess")
+// const errorNode = document.getElementById("error")
+
+function validateForm(){
+console.log("hello woekddsf");
+}
